b67bc881e809d1445b76ba109c689303a446a306
Ming-Jan (Randy) Pan <randy19981223@gmail.com>
2023-11-23 15:38:31 -0500

    EDA

---
+++ data/get_data.py
@@ -0,0 +1,34 @@
+"""Code to get the following data: 
+
+1. Walkability data for each state (at city level)
+2. CDC physical activity  
+
+"""
+
+import pandas as pd
+import requests
+from io import StringIO
+
+# WALKABILITY DATA
+list_us_abbv = pd.read_html("https://www.ssa.gov/international/coc-docs/states.html")[
+    0
+][1].to_list()
+
+df_walk = pd.DataFrame()
+for st in list_us_abbv:
+    print(st)
+    try:
+        temp = pd.read_html(f"https://www.walkscore.com/{st}")[0]
+        temp["State"] = st
+        df_walk = pd.concat([temp, df_walk])
+    except:
+        pass
+
+# excludes American Samoa (AS)
+assert df_walk["State"].nunique() == 51
+
+df_walk.drop(["Transit Score"], axis=1).to_csv("./data/walkability.csv")
+
+# PHYSICAL ACTIVITY DATA 
+response = requests.get("https://data.cdc.gov/resource/vba9-s8jp.csv").text
+data = StringIO(response)
